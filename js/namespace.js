2748184b0ed14961f2ddec6183bd0427cda7f29f
Andrii Raikov <andrii.raikov@savedo.de>
2019-07-02 10:02:07 +0200

    Fix wrongUpdateEvery

--- js/namespace.js
+++ js/namespace.js
@@ -7,10 +7,17 @@ function loadGithubConfigsFromStorage(callback = () => {}) {
   chrome.storage.local.get('githubConfigs', result => {
     backgrounds.githubConfigs = result.githubConfigs || { updateEvery: 60 };
 
-    if (backgrounds.githubConfigs.updateEvery < 60 || backgrounds.githubConfigs.updateEvery > 600) {
+    if (wrongUpdateEvery()) {
       backgrounds.githubConfigs.updateEvery = 60;
     }
 
     callback();
   });
+}
+
+function wrongUpdateEvery() {
+  return typeof(backgrounds.githubConfigs.updateEvery) === "undefined" ||
+    backgrounds.githubConfigs.updateEvery === null ||
+    backgrounds.githubConfigs.updateEvery < 60 ||
+    backgrounds.githubConfigs.updateEvery > 600
 }
