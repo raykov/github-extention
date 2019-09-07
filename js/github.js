01b5756e0d3952eabcdc7560501121a43c53a4fd
Andrii Raikov <raykov.andrey@gmail.com>
2019-09-07 09:59:23 +0200

    Build request params

--- js/github.js
+++ js/github.js
@@ -1,6 +1,6 @@
 function requestReviewRequests(callback = () => {}, onError = status => {}) {
   const baseUrl = "https://api.github.com/search/issues";
-  const url = `${baseUrl}?q=is:pr+is:open+review-requested:${backgrounds.githubConfigs.username}`;
+  const url = `${baseUrl}?q=${requestParams()}`;
 
   const request = new XMLHttpRequest();
   request.open('GET', url, true);
@@ -19,4 +19,19 @@ function requestReviewRequests(callback = () => {}, onError = status => {}) {
   };
 
   request.send();
+}
+
+function requestParams(){
+  let data = [];
+  // is:pr+is:open+review-requested:${backgrounds.githubConfigs.username}+user:raykov
+
+  data.push(`is:${backgrounds.githubConfigs.is}`);
+  data.push(`state:${backgrounds.githubConfigs.state}`);
+  data.push(`review-requested:${backgrounds.githubConfigs.username}`);
+  if (backgrounds.githubConfigs.user !== "") data.push(`user:${backgrounds.githubConfigs.user}`);
+  if (backgrounds.githubConfigs.repo !== "") data.push(`repo:${backgrounds.githubConfigs.repo}`);
+  if (backgrounds.githubConfigs.author !== "") data.push(`author:${backgrounds.githubConfigs.author}`);
+  if (backgrounds.githubConfigs.mentions !== "") data.push(`mentions:${backgrounds.githubConfigs.mentions}`);
+
+  return data.join("+")
 }
